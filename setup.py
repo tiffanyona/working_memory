5e67892d9a32c1bc02ead9083e7a111c494db271
tiffanyona <tiffany.ona@alleninstitute.org>
2025-01-20 21:07:53 -0800

    initialize repo

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="working_memory",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[  # List your dependencies here
+        "numpy",
+        "pandas",
+        "elephant",
+        "neo",
+        "sklearn",
+        "matplotlib",
+    ],
+)
